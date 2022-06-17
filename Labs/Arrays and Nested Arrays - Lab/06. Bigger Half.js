af41f65e139c51ccf3f24e8e7b07076be18ceaea
CraXWaR <craxwar@gmail.com>
2022-06-17 23:52:25 +0300

    Changes

--- Labs/Arrays and Nested Arrays - Lab/06. Bigger Half.js
+++
@@ -1,8 +0,0 @@
-function solve(input) {
-    input.sort((a, b) => a - b)
-    let part = Math.floor(input.length / 2)
-    let result = input.slice(part)
-
-    return result
-}
-console.log(solve([4, 7, 2, 5]));
--- Labs/Syntax, Functions and Statements - Lab/08. Days in a month.js
+++
@@ -1,5 +0,0 @@
-function solve(month, year) {
-    let day = new Date(year, month, 0).getDate();
-    console.log(day);
-}
-solve(1, 2012)
